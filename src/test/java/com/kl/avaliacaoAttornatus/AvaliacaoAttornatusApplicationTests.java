985e4308d34128f4f747fa77b19ceef0f2b46988
Kleber Martins <kleber@klsites.com.br>
2023-01-18 10:10:41 -0300

    projeto criado

---
+++ src/test/java/com/kl/avaliacaoAttornatus/AvaliacaoAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kl.avaliacaoAttornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
